54eb6c036c4ea34b4c9832265f223326fd8431d7
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-02-10 22:30:12 -0600

    Time: 32 ms (82.71%), Space: 14.6 MB (22.64%) - LeetHub

---
+++ subsets/subsets.py
@@ -0,0 +1,49 @@
+# class Solution:
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         self.count =  0
+#         def backtrack(first = 0, curr = []):
+#             # if the combination is done
+#             if len(curr) == k:  
+#                 output.append(curr[:])
+#             for i in range(first, n):
+#                 self.count += 1
+#                 # add nums[i] into the current combination
+#                 curr.append(nums[i])
+#                 # use next integers to complete the combination
+#                 backtrack(i + 1, curr)
+#                 # backtrack
+#                 curr.pop()
+        
+#         output = []
+#         n = len(nums)
+#         for k in range(n + 1):
+#             backtrack()
+#         print("Final output size: " + str(len(output)) + 
+#               ' vs # recursion calls: ' + str(self.count))
+#         return output
+
+class Solution:
+	def subsets(self, nums: List[int]) -> List[List[int]]:
+		# self.count = 0
+		self.ans = []
+		def n_choose_k(n, k, prev_combo):
+			for count, val in enumerate(n):
+				# self.count += 1
+				#stopping further exploration because 
+				#it's not possible to create large enough combinations anymore
+				if len(prev_combo) + len(n) - count < k:
+					return
+				if len(prev_combo) + 1 == k:
+					#appending only [val] instead of creating copy of list using [:]
+					#[:] creates shallow copy which takes O(N) time
+					self.ans.append(prev_combo + [val]) 
+					continue
+				n_choose_k(n[count + 1:], k, prev_combo + [val])
+
+		for k in range(1,len(nums) + 1):
+			n_choose_k(nums, k, [])
+
+		self.ans.append([])
+		# print("Final output size: " + str(len(self.ans)) + 
+		# 	  ' vs # recursion calls: ' + str(self.count))
+		return self.ans
