64e01b0c6575e211d772304cb898d7dd9bb85e61
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-02-13 14:45:07 -0600

    Time: 784 ms (65.24%), Space: 70.9 MB (37.03%) - LeetHub

--- first-unique-number/first-unique-number.py
+++ first-unique-number/first-unique-number.py
@@ -1,26 +1,30 @@
-from collections import deque
+from collections import OrderedDict
 class FirstUnique:
 
     def __init__(self, nums: List[int]):
         self.vals = dict()
-        self.Q = deque()
+        self.Q = OrderedDict()
         for val in nums:
             self.add(val)
 
     def showFirstUnique(self) -> int:
-        while self.Q:
-            if self.vals[self.Q[0]] == 0:
-                return self.Q[0]
-            else:
-                self.Q.popleft()
+        if self.Q:
+            return next(iter(self.Q))
         return -1
+        # while self.Q:
+        #     if self.vals[self.Q[0]] == 0:
+        #         return self.Q[0]
+        #     else:
+        #         self.Q.popleft()
+        # return -1
 
     def add(self, value: int) -> None:
         if value not in self.vals:
             self.vals[value] = 0
+            self.Q[value] = 1
         else:
             self.vals[value] = -1
-        self.Q.append(value)
+            if value in self.Q: del self.Q[value]
             
 
 
