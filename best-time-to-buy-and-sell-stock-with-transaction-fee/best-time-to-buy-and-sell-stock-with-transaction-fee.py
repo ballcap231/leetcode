1fea4c1818a368a8617463103686a68fed9c9b07
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-02-15 18:17:21 -0600

    Time: 616 ms (94.84%), Space: 21.3 MB (81.88%) - LeetHub

--- best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,8 +1,25 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        sold, bought = 0, -prices[0]
-        for day in range(1, len(prices)):
-            sold = max(sold, bought + prices[day] - fee)
-            bought = max(bought, sold - prices[day])
+        
+        #Greedy soln?
+        n = len(prices)
+        if n < 2:
+             return 0
+        ans = 0
+        minimum = prices[0]
+        for i in range(1, n):
+            if prices[i] < minimum:
+                minimum = prices[i]
+            elif prices[i] > minimum + fee:
+                ans += prices[i] - fee - minimum
+                minimum = prices[i] - fee
+        return ans
+    
+    
+#         #DP soln, O(N) time and O(1) space
+#         sold, bought = 0, -prices[0]
+#         for day in range(1, len(prices)):
+#             sold = max(sold, bought + prices[day] - fee)
+#             bought = max(bought, sold - prices[day])
             
-        return sold
+#         return sold
