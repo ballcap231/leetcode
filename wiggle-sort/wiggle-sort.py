64d7d8964e349387f3bb6141e929b55758f39d71
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-02-18 22:13:37 -0600

    Time: 92 ms (74.60%), Space: 15.1 MB (55.59%) - LeetHub

--- wiggle-sort/wiggle-sort.py
+++ wiggle-sort/wiggle-sort.py
@@ -3,5 +3,6 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        #O(N) time and O(1) space
         for ii in range(len(nums)):
             nums[ii:ii+2] = sorted(nums[ii:ii+2], reverse = ii % 2) 
