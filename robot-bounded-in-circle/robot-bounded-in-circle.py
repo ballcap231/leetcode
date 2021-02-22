4c94e2c2fe3856dc45ebb4afbfdbb61194564c42
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-02-21 19:59:32 -0600

    Time: 32 ms (65.63%), Space: 14.3 MB (20.74%) - LeetHub

--- robot-bounded-in-circle/robot-bounded-in-circle.py
+++ robot-bounded-in-circle/robot-bounded-in-circle.py
@@ -1,5 +1,9 @@
 class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
+        #O(N) time and O(1) space
+# the robot is already back to its origin by the end of the string traversal, and
+# the robot is away from the origin, but heading to a direction different from its initial direction. For example, if the robot is facing left by the end of the first string traversal, after three other traversals of left->left->left, it is back to the origin. A second example is that if the robot is facing down by the end of the first string traversal, it only takes another traversal for it to get back to the origin.
+
         directions = [[0,1],[1,0],[0,-1],[-1,0]]
         direction = 0 # 0,1,2,3,for N,E,S,W respectively
         pos = [0,0]
