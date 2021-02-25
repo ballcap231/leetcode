6a4bea3c00ac6f63aa138b64fea65980beb1cc60
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-02-24 21:48:45 -0600

    Time: 184 ms (83.98%), Space: 14.6 MB (73.32%) - LeetHub

--- number-of-provinces/number-of-provinces.py
+++ number-of-provinces/number-of-provinces.py
@@ -1,17 +1,40 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         #O(N^2) time and O(N) space
-        #DFS
-        seen = set()
-        def dfs(node):
-            for count, val in enumerate(isConnected[node]):
-                if val and count not in seen:
-                    seen.add(count)
-                    dfs(count)
-        ans = 0
-        for val in range(len(isConnected)):
-            if val not in seen:
-                dfs(val)
-                ans += 1
+        #Recursive DFS
         
-        return ans
+        # seen = set()
+        # def dfs(node):
+        #     for count, val in enumerate(isConnected[node]):
+        #         if val and count not in seen:
+        #             seen.add(count)
+        #             if count != node:
+        #                 dfs(count)
+        # ans = 0
+        # for val in range(len(isConnected)):
+        #     if val not in seen:
+        #         dfs(val)
+        #         ans += 1
+        # return ans
+    
+        #Iterative BFS
+        #O(N^2) time and O(N) space
+        
+        seen = set()
+        self.qq = deque()
+        def dfs(kk):
+            self.qq.append(kk)
+            while self.qq:
+                popped = self.qq.popleft()
+                for pos, is_one in enumerate(isConnected[popped]):
+                    if is_one and pos not in seen:
+                        seen.add(pos)
+                        if pos != kk:
+                            self.qq.append(pos)
+        tot = 0
+        for nn in range(len(isConnected)):
+            if nn not in seen:
+                dfs(nn)
+                tot += 1
+        return tot
+        
