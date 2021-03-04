2548702fd9001e63c2759790df1a521163894a6d
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-03-03 19:58:55 -0600

    Time: 96 ms (88.68%), Space: 18.5 MB (5.47%) - LeetHub

--- number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
+++ number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
@@ -1,6 +1,7 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         #O(max(n,N)) time and O(N) space where N is length of edges
+        #using DFS or BFS
         dd = defaultdict(set)
         seen = set()
         for a, b in edges:
@@ -8,6 +9,7 @@ class Solution:
             dd[b].add(a)
         
         def dfs(node):
+            seen.add(node)
             for ii in dd[node]:
                 if ii not in seen:
                     seen.add(ii)
@@ -15,6 +17,20 @@ class Solution:
             return 1
         
         return sum(dfs(ii) for ii in range(n) if ii not in seen)
+    
+    
+#         def bfs(node):
+#             seen.add(node)
+#             qq = deque(dd[node])
+#             while qq:
+#                 nn = qq.popleft()
+#                 if nn not in seen:
+#                     seen.add(nn)
+#                     for jj in dd[nn]: qq.append(jj)
+#             return 1
+        
+#         return sum(bfs(ii) for ii in range(n) if ii not in seen)
+        
             
             
             
