56f660956cfca3d9b7b64e0eeeb65f960abe6ff7
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-03-10 16:19:44 -0600

    Time: 32 ms (55.76%), Space: 14.4 MB (13.58%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        @lru_cache(maxsize = None)
+        def recurse(n):
+            if n == 1:
+                return 1
+            if n == 2:
+                return 2
+
+            return recurse(n - 1) + recurse(n - 2)
+        
+        return recurse(n)
