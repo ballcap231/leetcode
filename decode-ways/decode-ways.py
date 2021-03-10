4b548d550f7422f9f7f4ecabbcde09a29d01eb58
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-03-10 15:41:50 -0600

    Time: 36 ms (40.81%), Space: 14.2 MB (86.88%) - LeetHub

--- decode-ways/decode-ways.py
+++ decode-ways/decode-ways.py
@@ -1,21 +1,35 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         
-        @lru_cache(maxsize = None)
-        def dfs(start):
-            if start == len(s):
-                return 1
-            
-            if s[start] == '0':
-                return 0
-            
-            ret = 0
-            
-            ret += dfs(start + 1)
-            
-            if 10 <= int(s[start:start + 2]) <= 26:
-                ret += dfs(start + 2)
-            
-            return ret
+#         @lru_cache(maxsize = None)
+#         def dfs(start):
+#             #postorder DFS
+#             if start == len(s):
+#                 return 1
+#             if s[start] == '0':
+#                 return 0
+#             ret = 0
+#             ret += dfs(start + 1)
+#             if 10 <= int(s[start:start + 2]) <= 26:
+#                 ret += dfs(start + 2)
+#             return ret
+#         return dfs(0)
+
+        if not s:
+            return 0
+        dp = [0 for _ in range(len(s) + 1)]
+    
+        dp[0] = 1
+        dp[1] = 1 if s[0] != '0' else 0
         
-        return dfs(0)
+        for ii in range(2, len(dp)):
+            
+            if s[ii - 1] != '0':
+                dp[ii] = dp[ii - 1]
+            
+            ints = int(s[ii-2: ii])
+            if 10 <= ints <= 26:
+                dp[ii] += dp[ii - 2]
+            
+        return dp[len(s)]
+        
