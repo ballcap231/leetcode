4f654ba433392ebfd60867552fea994ce22db1ca
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-03-14 18:48:55 -0500

    Time: 52 ms (90.86%), Space: 14.6 MB (29.44%) - LeetHub

---
+++ slowest-key/slowest-key.py
@@ -0,0 +1,11 @@
+class Solution:
+    def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
+        slowest_key = keysPressed[0]
+        slowest_time = releaseTimes[0]
+        for prev_pos, time in enumerate(releaseTimes[1:]):
+            duration = time - releaseTimes[prev_pos]
+            if duration > slowest_time or \
+                (duration == slowest_time and keysPressed[prev_pos + 1] > slowest_key):
+                slowest_key = keysPressed[prev_pos + 1]
+                slowest_time = duration
+        return slowest_key
