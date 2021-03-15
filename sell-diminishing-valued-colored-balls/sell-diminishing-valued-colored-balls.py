ab5f39d7ac924030e410d52642301c94faa834ba
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-03-15 00:18:01 -0500

    Time: 720 ms (70.90%), Space: 35.4 MB (11.69%) - LeetHub

--- sell-diminishing-valued-colored-balls/sell-diminishing-valued-colored-balls.py
+++ sell-diminishing-valued-colored-balls/sell-diminishing-valued-colored-balls.py
@@ -4,10 +4,11 @@ class Solution:
         ans, ind, width=0,0,0
         
         while orders>0:
-            width += arr[ind][1]
-            sell=min(orders, width * (arr[ind][0] - arr[ind+1][0]))
+            ball_val,ball_count = arr[ind][0], arr[ind][1]
+            width += ball_count
+            sell=min(orders, width * (ball_val - arr[ind+1][0]))
             whole, remainder= divmod(sell, width)
-            ans += width*(whole*(arr[ind][0]+arr[ind][0]-(whole-1)))//2 + remainder*(arr[ind][0]-whole)
+            ans += width*(whole*(ball_val + ball_val -(whole-1)))//2 + remainder*(ball_val-whole)
             orders -= sell
             ind += 1
         return ans % 1_000_000_007  
@@ -15,32 +16,5 @@ class Solution:
         
         
         
-        
-        
-        
-        
-        
-        
-        
 
-        
-        
-#         hq = []
-#         profit = 0
-#         for ball in inventory:
-#             heapq.heappush(hq, -ball)
-        
-#         while orders:
-#             ball_val = -heapq.heappop(hq)
-#             all_ball_val = orders
-#             if hq:
-#                 all_ball_val = (ball_val + hq[0] + 1) // 2 * (ball_val - hq[0])
-            
-#             next_profit = min(all_ball_val, orders)
-#             profit += next_profit
-#             new_ball_val = ball_val - next_profit
-#             if new_ball_val > 0:
-#                 heapq.heappush(hq, -new_ball_val)
-#             orders -= next_profit
-#         return profit % (10 ** 9 + 7)
             
