ee899bb2a4aa2053cfa9c24227e228a03881f9a0
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-03-15 23:56:12 -0500

    Time: 748 ms (59.70%), Space: 36.2 MB (6.96%) - LeetHub

--- sell-diminishing-valued-colored-balls/sell-diminishing-valued-colored-balls.py
+++ sell-diminishing-valued-colored-balls/sell-diminishing-valued-colored-balls.py
@@ -1,36 +1,21 @@
 class Solution:
     def maxProfit(self, inv: List[int], orders: int) -> int:
+        #O(NlogN + |inv|) time and O(N) space complexity where N is the # of unique numbers in inv 
+        #|inv| represents the length of inv
         counts = sorted(Counter(inv).items(), reverse = True) + [(0,0)]
         ans,width, ind = 0,0,0
-        
         while orders > 0:
             ball_val, ball_count = counts[ind][0], counts[ind][1]
             width += ball_count
             balls_to_add = min(orders, width * (ball_val - counts[ind + 1][0]))
             whole, remainder = divmod(balls_to_add, width)
-            ball_vals_to_add = width * (whole * (ball_val + ball_val - (whole - 1))) // 2 + \
+            #Can't use int(my_number / 2) if numbers are too big. Longs would give inprecise values
+            #since this uses floating-point arithmetic (i.e (IEEE-754 64-bit))
+            #Must used floating-point division (i.e. my_number // 2)
+            ball_vals_to_add = (width * (whole * (ball_val + ball_val - (whole - 1)))) // 2 + \
                                 remainder * (ball_val - whole)
             ans += ball_vals_to_add
             orders -= balls_to_add
             ind += 1
         return ans % 1_000_000_007
-        
-#         arr=sorted(Counter(inv).items(), reverse=True)+[(0,0)]
-#         ans, ind, width=0,0,0
-        
-#         while orders>0:
-#             ball_val,ball_count = arr[ind][0], arr[ind][1]
-#             width += ball_count
-#             sell=min(orders, width * (ball_val - arr[ind+1][0]))
-#             whole, remainder= divmod(sell, width)
-#             #arithmetic sum
-#             ans += width*(whole*(ball_val + ball_val -(whole-1)))//2 + remainder*(ball_val-whole)
-#             orders -= sell
-#             ind += 1
-#         return ans % 1_000_000_007
-        
-        
-        
-        
-
             
