918089dfc782eef61c23053a730f17856bf7832e
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-03-17 19:20:10 -0500

    Time: 216 ms (76.04%), Space: 17 MB (61.47%) - LeetHub

--- number-of-distinct-islands/number-of-distinct-islands.py
+++ number-of-distinct-islands/number-of-distinct-islands.py
@@ -4,30 +4,53 @@ class Solution:
         islands = set()
         x_len,y_len = len(grid), len(grid[0])
         def check_bounds(xx,yy):
-            if -1 < xx < x_len and -1 < yy < y_len:
+            if -1 < xx < x_len and -1 < yy < y_len and grid[xx][yy]:
                 return True
             return False
         dirs = [(1,0),(-1,0),(0,1),(0,-1)]
         
+        #BFS SOLUTION
         
-        def bfs(xx,yy):
-            deq = deque()
-            deq.append((xx,yy))
-            land = []
-            while deq:
-                pos_x, pos_y = deq.popleft()
-                for x_dir, y_dir in dirs:
-                    new_x, new_y = pos_x + x_dir, pos_y + y_dir
-                    if not check_bounds(new_x, new_y) or not grid[new_x][new_y]:
-                        continue
-                    deq.append((new_x,new_y))
-                    grid[new_x][new_y] = 0
-                    land.append((new_x - xx, new_y - yy))
-            islands.add(tuple(land))
-                    
+        # def bfs(xx,yy):
+        #     deq = deque()
+        #     deq.append((xx,yy))
+        #     land = []
+        #     while deq:
+        #         pos_x, pos_y = deq.popleft()
+        #         for x_dir, y_dir in dirs:
+        #             new_x, new_y = pos_x + x_dir, pos_y + y_dir
+        #             if not check_bounds(new_x, new_y):
+        #                 continue
+        #             deq.append((new_x,new_y))
+        #             grid[new_x][new_y] = 0
+        #             land.append((new_x - xx, new_y - yy))
+        #     islands.add(tuple(land))
+
+        # for xx in range(x_len):
+        #     for yy in range(y_len):
+        #         if grid[xx][yy]:
+        #             bfs(xx,yy)
+        
+        # return len(islands)
+        
+        #DFS SOLUTION
+        
+        def dfs(xx,yy):
+            if not check_bounds(xx,yy):
+                return
+            grid[xx][yy] = 0
+            self.island.append((xx - self.first_x, yy - self.first_y))
+            dfs(xx - 1, yy)
+            dfs(xx + 1, yy)
+            dfs(xx, yy - 1)
+            dfs(xx, yy + 1)
+            
         for xx in range(x_len):
             for yy in range(y_len):
                 if grid[xx][yy]:
-                    bfs(xx,yy)
-                
+                    self.island = []
+                    self.first_x, self.first_y = xx,yy
+                    dfs(xx,yy)
+                    islands.add(tuple(self.island))
+
         return len(islands)
