851a2b5c37c2b270642e1b640a3873356ef99ab7
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-03-19 18:13:34 -0500

    Time: 40 ms (66.53%), Space: 14.1 MB (95.06%) - LeetHub

---
+++ prison-cells-after-n-days/prison-cells-after-n-days.py
@@ -0,0 +1,33 @@
+class Solution:
+    def prisonAfterNDays(self, cells: List[int], n: int) -> List[int]:
+        def next_day(cells):
+            new_cell = []
+            for pos in range(len(cells)):
+                if pos == 0 or pos == len(cells) - 1 or \
+                    (cells[pos - 1] ^ cells[pos + 1]):
+                    new_cell.append(0)
+                else:
+                    new_cell.append(1)
+            return new_cell
+        
+        prev_cells = {tuple(cells):n}
+        
+        while n > 0:
+            cells = tuple(next_day(cells))
+            
+            if cells in prev_cells:
+                n -= 1
+                cycle_len = prev_cells[cells] - n
+                quot, rem = divmod(n, cycle_len)
+                n = rem
+                break
+            n -= 1
+            prev_cells[cells] = n
+            
+        
+        for ii in range(n):
+            cells = next_day(cells)
+        return cells
+
+        
+        
