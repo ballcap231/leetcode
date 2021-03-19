9ea2c26b274f048cbdf2c3c7f39774ffc43face4
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-03-18 22:10:20 -0500

    Time: 120 ms (72.59%), Space: 14.3 MB (83.62%) - LeetHub

--- snakes-and-ladders/snakes-and-ladders.py
+++ snakes-and-ladders/snakes-and-ladders.py
@@ -1,5 +1,7 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
+        #O(N^2) time and O(N^2) space because board is a N x N square 
+        #using BFS
         N = len(board)
         
         deq = deque()
@@ -7,44 +9,19 @@ class Solution:
         pos_moves = {1:0}
         def get_pos(pos):
             quot, rem = divmod(pos - 1, N)
-            
             row = N - 1 - quot
             col = rem if row%2 != N%2 else N - rem - 1
             return row, col
         
         while deq:
             pos = deq.popleft()
-            if pos == N * N:
-                return pos_moves[pos]
             for pos_2 in range(pos + 1, min(N*N + 1, pos + 7)):
-
                 row, col = get_pos(pos_2)
                 if board[row][col] > -1:
                     pos_2 = board[row][col]
-                if pos_2 in pos_moves:
-                    continue
-                deq.append(pos_2)
-                pos_moves[pos_2] = pos_moves[pos] + 1
-                
+                if pos_2 == N * N:
+                    return pos_moves[pos] + 1
+                if pos_2 not in pos_moves:
+                    deq.append(pos_2)
+                    pos_moves[pos_2] = pos_moves[pos] + 1
         return -1
-#         N = len(board)
-
-#         def get(s):
-#             # Given a square num s, return board coordinates (r, c)
-#             quot, rem = divmod(s-1, N)
-#             row = N - 1 - quot
-#             col = rem if row%2 != N%2 else N - 1 - rem
-#             return row, col
-#         dist = {1: 0}
-#         queue = collections.deque([1])
-#         while queue:
-#             s = queue.popleft()
-#             if s == N*N: return dist[s]
-#             for s2 in range(s+1, min(s+6, N*N) + 1):
-#                 r, c = get(s2)
-#                 if board[r][c] != -1:
-#                     s2 = board[r][c]
-#                 if s2 not in dist:
-#                     dist[s2] = dist[s] + 1
-#                     queue.append(s2)
-#         return -1
