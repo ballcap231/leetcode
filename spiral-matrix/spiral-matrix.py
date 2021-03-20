4572b6be796e3c694cd3d0e9719d7573f5bd3666
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-03-19 21:35:31 -0500

    Time: 32 ms (63.03%), Space: 14.4 MB (32.88%) - LeetHub

--- spiral-matrix/spiral-matrix.py
+++ spiral-matrix/spiral-matrix.py
@@ -1,31 +1,63 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        dirs = [[0,1],[1,0],[0,-1],[-1,0]]
-        x_len,y_len = len(matrix), len(matrix[0])
-        pos = [0,0]
-        dir_pos = 0
+        dirs = [(0,1),(1,0),(0,-1),(-1,0)]
+        dir_pos = curr_x = curr_y = 0
         curr_dir = dirs[dir_pos]
-        ret= []
-        
-        def valid_pos(pos):
-            if -1 < pos[0] < x_len and -1 < pos[1] < y_len and \
-                matrix[pos[0]][pos[1]] is not None:
+        x_len,y_len = len(matrix), len(matrix[0])
+        ans = []
+        def valid_pos(xx, yy):
+            if -1 < xx < x_len and -1 < yy < y_len and \
+                matrix[xx][yy] is not None:
                 return True
             return False
         
-        while valid_pos(pos):
-            while valid_pos(pos):
-                ret.append(matrix[pos[0]][pos[1]])
-                matrix[pos[0]][pos[1]] = None
-                pos[0] += curr_dir[0]
-                pos[1] += curr_dir[1]
-            pos[0] -= curr_dir[0]
-            pos[1] -= curr_dir[1]
+        while valid_pos(curr_x, curr_y):
+            while valid_pos(curr_x, curr_y):
+                ans.append(matrix[curr_x][curr_y])
+                matrix[curr_x][curr_y] = None
+                curr_x += curr_dir[0]
+                curr_y += curr_dir[1]
+            curr_x -= curr_dir[0]
+            curr_y -= curr_dir[1]
             dir_pos = (dir_pos + 1) % 4
             curr_dir = dirs[dir_pos]
-            pos[0] += curr_dir[0]
-            pos[1] += curr_dir[1]
-        return ret
+            curr_x += curr_dir[0]
+            curr_y += curr_dir[1]
+        return ans
+        
+        
+        
+        
+        
+        
+        
+        
+#         dirs = [[0,1],[1,0],[0,-1],[-1,0]]
+#         x_len,y_len = len(matrix), len(matrix[0])
+#         pos = [0,0]
+#         dir_pos = 0
+#         curr_dir = dirs[dir_pos]
+#         ret= []
+        
+#         def valid_pos(pos):
+#             if -1 < pos[0] < x_len and -1 < pos[1] < y_len and \
+#                 matrix[pos[0]][pos[1]] is not None:
+#                 return True
+#             return False
+        
+#         while valid_pos(pos):
+#             while valid_pos(pos):
+#                 ret.append(matrix[pos[0]][pos[1]])
+#                 matrix[pos[0]][pos[1]] = None
+#                 pos[0] += curr_dir[0]
+#                 pos[1] += curr_dir[1]
+#             pos[0] -= curr_dir[0]
+#             pos[1] -= curr_dir[1]
+#             dir_pos = (dir_pos + 1) % 4
+#             curr_dir = dirs[dir_pos]
+#             pos[0] += curr_dir[0]
+#             pos[1] += curr_dir[1]
+#         return ret
             
         
         
