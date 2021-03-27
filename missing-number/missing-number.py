742a837a4020bb20006a6669d1b999dadcaf514f
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-03-27 10:18:19 -0500

    Time: 192 ms (15.26%), Space: 15.5 MB (51.93%) - LeetHub

--- missing-number/missing-number.py
+++ missing-number/missing-number.py
@@ -1,5 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        #O(N) time and O(1) space
         #arithmetic sum - n/2 (a1 + an)
         
         arthmetic_sum = (len(nums) + 1) * (0 + len(nums)) // 2
