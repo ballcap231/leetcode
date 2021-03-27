1558535d50a4d51182b0d8e8e01523443f3f3208
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-03-27 12:54:06 -0500

    Time: 36 ms (81.58%), Space: 14.8 MB (7.49%) - LeetHub

--- word-break/word-break.py
+++ word-break/word-break.py
@@ -1,32 +1,51 @@
-class Node:
-    def __init__(self):
-        self.children = dict()
-        self.is_word = False
-class Trie:
-    def __init__(self):
-        self.root = Node()
-    def add_word(self, word):
-        node = self.root
-        for char in word:
-            node = node.children.setdefault(char, Node())
-        node.is_word = True
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        trie = Trie()
-        for word in wordDict:
-            trie.add_word(word)
+        len_word = len(s)
+        dictionary = set(wordDict)
+
         @lru_cache(maxsize=None)
-        def valid_substr(start):
-            if start >= len(s):
+        def check_substring(start):
+            if start == len_word:
                 return True
-            node = trie.root
-            for pos in range(start, len(s)):
-                if s[pos] not in node.children:
-                    return False
-                node = node.children[s[pos]]
-                if node.is_word and valid_substr(pos + 1):
+            for curr_pos in range(start + 1, len_word + 1):
+                if s[start:curr_pos] in dictionary and check_substring(curr_pos):
                     return True
-        return valid_substr(0)
+            return False
+        
+        return check_substring(0)
+
+
+
+
+# class Node:
+#     def __init__(self):
+#         self.children = dict()
+#         self.is_word = False
+# class Trie:
+#     def __init__(self):
+#         self.root = Node()
+#     def add_word(self, word):
+#         node = self.root
+#         for char in word:
+#             node = node.children.setdefault(char, Node())
+#         node.is_word = True
+# class Solution:
+#     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+#         trie = Trie()
+#         for word in wordDict:
+#             trie.add_word(word)
+#         @lru_cache(maxsize=None)
+#         def valid_substr(start):
+#             if start >= len(s):
+#                 return True
+#             node = trie.root
+#             for pos in range(start, len(s)):
+#                 if s[pos] not in node.children:
+#                     return False
+#                 node = node.children[s[pos]]
+#                 if node.is_word and valid_substr(pos + 1):
+#                     return True
+#         return valid_substr(0)
                 
                 
             
