bcdd9eb1b65a7a936f4de8398ed23f225d94b43e
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-03-30 19:31:39 -0500

    Time: 460 ms (72.24%), Space: 16.2 MB (50.58%) - LeetHub

--- network-delay-time/network-delay-time.py
+++ network-delay-time/network-delay-time.py
@@ -4,43 +4,44 @@ class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         #Djikstra's Algorithm - 
         
-        graph = collections.defaultdict(list)
-        for u, v, w in times:
-            graph[u].append((v, w))
+#         graph = collections.defaultdict(list)
+#         for u, v, w in times:
+#             graph[u].append((v, w))
 
-        pq = [(0, k)]
-        dist = {}
-        while pq:
-            d, node = heapq.heappop(pq)
-            if node in dist: continue
-            dist[node] = d
-            for nei, d2 in graph[node]:
-                if nei not in dist:
-                    heapq.heappush(pq, (d+d2, nei))
+#         pq = [(0, k)]
+#         dist = {}
+#         while pq:
+#             d, node = heapq.heappop(pq)
+#             if node in dist: continue
+#             dist[node] = d
+#             for nei, d2 in graph[node]:
+#                 if nei not in dist:
+#                     heapq.heappush(pq, (d+d2, nei))
 
-        return max(dist.values()) if len(dist) == n else -1
+#         return max(dist.values()) if len(dist) == n else -1
         
         
-#         adj_list = defaultdict(list)
-#         for source, target, time in times:
-#             adj_list[source].append((target, time))
+        adj_list = defaultdict(list)
+        for source, target, time in times:
+            adj_list[source].append((target, time))
         
-#         fastest_time = {node: float('inf') for node in range(1, n + 1)}
-#         fastest_time[k] = 0
-#         max_time = 0
-#         seen = set()
-#         to_visit = [(0,k)]
-#         while to_visit:
-#             time_from_origin, node = heapq.heappop(to_visit)
-#             seen.add(node)
-#             for neighbor, time in adj_list[node]:
-#                 if neighbor not in seen:
-#                     new_neighbor_time = min(fastest_time[neighbor], time_from_origin + time)
-#                     fastest_time[neighbor] = new_neighbor_time
-#                     if neighbor not seen:
-#                         heapq.heappush(to_visit, (new_neighbor_time , neighbor))
-            
-            # max_time = max(max_time, time_from_origin)
+        fastest_time = {node: float('inf') for node in range(1, n + 1)}
+        fastest_time[k] = 0
+        max_time = 0
+        seen = set()
+        to_visit = [(0,k)]
+        while to_visit:
+            time_from_origin, node = heapq.heappop(to_visit)
+            if node not in seen:
+                seen.add(node)
+                for neighbor, time in adj_list[node]:
+                    if neighbor not in seen:
+                        new_neighbor_time = min(fastest_time[neighbor], time_from_origin + time)
+                        fastest_time[neighbor] = new_neighbor_time
+                        if neighbor not in seen:
+                            heapq.heappush(to_visit, (new_neighbor_time , neighbor))
+
+                max_time = max(max_time, time_from_origin)
         
-#         max_time = max(fastest_time.values())
-#         return max_time if all((time != float('inf') for time in fastest_time.values())) else -1
+        max_time = max(fastest_time.values())
+        return max_time if all((time != float('inf') for time in fastest_time.values())) else -1
