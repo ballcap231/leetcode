c9faded1430eb549de34b6cff25e9eaadc882afb
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-04-01 23:53:14 -0500

    Time: 32 ms (67.84%), Space: 14.1 MB (89.78%) - LeetHub

--- cousins-in-binary-tree/cousins-in-binary-tree.py
+++ cousins-in-binary-tree/cousins-in-binary-tree.py
@@ -6,14 +6,16 @@
 #         self.right = right
 class Solution:
     def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
+        #O(N) time and O(N) space
+        if root and (root.val == x or root.val == y): return False
+        
         def get_node_level(curr_node, search_node, parent_node=None, level = 0):
             if not curr_node: return None
             if curr_node.val == search_node:
                 return (level, parent_node.val)
             return get_node_level(curr_node.left, search_node, curr_node, level + 1) or \
                             get_node_level(curr_node.right, search_node, curr_node, level + 1)
-            
-        if root and (root.val == x or root.val == y): return False
+
         x_level, x_parent = get_node_level(root, x)
         y_level, y_parent = get_node_level(root, y)
         return x_level == y_level and y_parent != x_parent
