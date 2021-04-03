e6323d589c1a1303517fe2132a4d761bffdde4ba
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-04-03 16:26:09 -0500

    Time: 804 ms (16.32%), Space: 19 MB (23.93%) - LeetHub

--- find-the-town-judge/find-the-town-judge.py
+++ find-the-town-judge/find-the-town-judge.py
@@ -1,6 +1,8 @@
-from collections import defaultdict
 class Solution:
     def findJudge(self, N: int, trust: List[List[int]]) -> int:
+        #O(E) time where E is number of edges
+        #O(N) space
+        if len(trust) < N - 1: return -1
         candidates = set(xx for xx in range(1, N + 1))
         
         outbound = defaultdict(set)
@@ -15,37 +17,3 @@ class Solution:
             if len(inbound[cand]) == N - 1:
                 return cand
         return -1
-        
-        
-        
-#         #if only one guy, he must be the judge 
-#         if N == 1:
-#             return 1
-        
-#         trustees = defaultdict(int)
-#         trusters = defaultdict(int)
-
-        
-#         for t_pair in trust:
-#             trustees[t_pair[1]] += 1
-#             trusters[t_pair[0]] += 1
-            
-#         set_trustees = set(trustees.keys())
-#         set_trusters = set(trusters.keys())
-#         #relative complement of for trustees must contain only the judge
-#         trustees_relative_complement = set_trustees - set_trusters
-        
-#         # town_judge = next(iter(trustees_relative_complement))
-        
-#         #can't index a set so iterate it once instead (for loop is apparently fastest way but next(iter()) is fast too but StopIteration Error is called if set is empty which is not what we want)
-#         for ii in trustees_relative_complement:
-#             town_judge = ii
-        
-#         #if the relative complement doesn't equal 1, then there's either no judge (less than 1) or more than 1 possible judges(greater than 1)
-#         if len(trustees_relative_complement) != 1:
-#             return -1
-#         #if the number of people who trust the potential judge does not match the number of trusters, then the judge is one of the trusters or not all the trusters trust the judge
-#         if trustees[town_judge] != len(set_trusters):
-#             return -1
-        
-#         return town_judge
