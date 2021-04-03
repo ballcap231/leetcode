4af468e374bf6d63fc3dc3d7393ddba30907e6d6
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-04-03 14:16:53 -0500

    Time: 36 ms (34.60%), Space: 14.4 MB (20.36%) - LeetHub

--- binary-tree-right-side-view/binary-tree-right-side-view.py
+++ binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -5,42 +5,48 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def rightSideView(self, root: TreeNode) -> List[int]:
-        #O(N) time and O(W) space where W is the largest width of the tree 
-        if not root: return []
-        ans = []
-        deq = deque()
-        deq.append((root, 0))
-        last_level = 0
-        
-        while deq:
-            node, level = deq.popleft()
+    def DFS(self, node, level):
+        if node:
+            # if level not in self.ans: self.ans[level] = node.val #works if we swap order of visiting right/left
+            self.ans[level] = node.val
+            self.DFS(node.left, level + 1)
+            self.DFS(node.right, level + 1)
             
-            if level > last_level:
-                ans.append(level_node.val)
-            last_level = level
-            level_node = node
-            if node.left: deq.append((node.left, level + 1))
-            if node.right: deq.append((node.right, level + 1))
+            
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        #pre-order DFS - O(N) time, O(logN) space on average and O(N) worst case
+        self.ans = {}
+        self.DFS(root, 0)
+        return list(self.ans.values())
+            
+            
+            
+            
+            
         
-        ans.append(level_node.val)
-        return ans
         
         
         
-#         queue = deque()
-#         queue.append(root)
-#         outPut = []
         
-#         while queue:
-#             n = len(queue)
-#             currLevel = []
-#             for _ in range(n):
-#                 node = queue.popleft()
-#                 currLevel.append(node.val)
-#                 if node.left:
-#                     queue.append(node.left)
-#                 if node.right:
-#                     queue.append(node.right)
-#             outPut.append(currLevel[-1])
-#         return outPut
+        
+        
+        
+#         #O(N) time and O(W) space where W is the largest width of the tree 
+#         if not root: return []
+#         ans = []
+#         deq = deque()
+#         deq.append((root, 0))
+#         last_level = 0
+        
+#         while deq:
+#             node, level = deq.popleft()
+            
+#             if level > last_level:
+#                 ans.append(level_node.val)
+#             last_level = level
+#             level_node = node
+#             if node.left: deq.append((node.left, level + 1))
+#             if node.right: deq.append((node.right, level + 1))
+        
+#         ans.append(level_node.val)
+#         return ans
