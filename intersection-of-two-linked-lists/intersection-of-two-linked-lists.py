9fa04271e534ceb8c271e7f2d0758d6ec41c23f8
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-04-03 22:14:25 -0500

    Time: 168 ms (41.21%), Space: 29.3 MB (83.51%) - LeetHub

--- intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -6,15 +6,53 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
-        nodes = set()
-        a_node = headA
-        b_node = headB
-        while a_node:
-            nodes.add(a_node)
-            a_node = a_node.next
+        #O(N + M) time where N and M are the lengths of the 2 lists
+        #O(1) space
+        #similar prob: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-iii/submissions/
+        pA = headA
+        pB = headB
         
-        while b_node:
-            if b_node in nodes: return b_node
-            b_node = b_node.next
+        while pA != pB:
+            pA = pA.next if pA is not None else headB
+            pB = pB.next if pB is not None else headA
+        return pA
         
-        return None
+        
+        
+#         #O(|A| + |B|) time and O(1) space
+#         curr_a, curr_b = headA, headB
+#         a_passes, b_passes = 0, 0
+        
+#         while curr_a != curr_b:
+#             if b_passes + a_passes > 2:
+#                 return None
+#             if curr_a.next:
+#                 curr_a = curr_a.next
+#             else:
+#                 curr_a = headB
+#                 a_passes += 1
+#             if curr_b.next:
+#                 curr_b = curr_b.next
+#             else:
+#                 curr_b = headA
+#                 b_passes += 1
+#         return curr_a
+            
+        
+        
+        
+        
+        
+#         #O(N) time and O(N) space where N = |A| + |B|
+#         nodes = set()
+#         a_node = headA
+#         b_node = headB
+#         while a_node:
+#             nodes.add(a_node)
+#             a_node = a_node.next
+        
+#         while b_node:
+#             if b_node in nodes: return b_node
+#             b_node = b_node.next
+        
+#         return None
