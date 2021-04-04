97d79b4a85c0645458d6b77afa4ef2c38fdb3c3f
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-04-03 21:01:14 -0500

    Time: 96 ms (5.73%), Space: 18.4 MB (61.71%) - LeetHub

--- lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.py
+++ lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -18,7 +18,31 @@ class Solution:
             n_2 = n_2.parent if n_2.parent else p
         
         return n_1
-        
+    
+    
+#Explanation behind solution above:  
+# This is so simple it seems stupid...
+
+# For anyone struggling to see it, imagine to nodes, p and q, whose paths merge and become common after a certain number of steps.
+# p1 -> p2 -> p3 -> c1 -> c2 -> c3
+# q1 -> q2 -> q3 -> c1 -> c2 -> c3
+# If the distance from p1 to c1 is the same as the distance from q1 to c1, it's pretty obvious this algorithm will find when c1 == c1.
+
+# But now imagine those distances are different.
+# p1 -> p2 -> p3 -> c1 -> c2 -> c3
+# .........................q1 -> c1 -> c2 -> c3
+
+# If you force them to switch paths after they reach c3:
+# P Travels: (3 steps to c1), (3 common steps to q1), (1 step to c1)
+# Q Travels: (1 step to c1), (3 common steps to p1), (3 steps to c1)
+
+# OR put another way
+
+# P Travels: PC, C, QC
+# Q Travels: QC, C, PC
+
+# where C is the common paths. PC is p's unique path to the common ancestor. QC is q's unique path.
+
         # p1, q1 = p, q
         # while p1 != q1:
         #     p1 = p1.parent if p1.parent else q
