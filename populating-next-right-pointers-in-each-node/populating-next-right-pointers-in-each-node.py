33bcc5d61703ca9ba427570347191281236c938f
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-04-07 22:33:58 -0500

    Time: 56 ms (92.20%), Space: 15.7 MB (31.25%) - LeetHub

--- populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
+++ populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -11,16 +11,66 @@ class Node:
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
         
-        deq = deque()
-        if root: deq.append(root)
-        prev_node = None
-        while deq:
-            for ii in range(len(deq)):
-                curr_node = deq.popleft()
-                if curr_node.left: deq.append(curr_node.left)
-                if curr_node.right: deq.append(curr_node.right)
-                if prev_node: prev_node.next = curr_node
-                prev_node = curr_node
-            curr_node.next = None
-            prev_node = None
+        curr_node = root
+        first_node = root
+        while first_node:
+            first_node = first_node.left
+            while curr_node:
+                if curr_node.left:
+                    curr_node.left.next = curr_node.right
+                if curr_node.right and curr_node.next:
+                    curr_node.right.next = curr_node.next.left
+                curr_node = curr_node.next
+            curr_node = first_node
+        
         return root
+        
+        
+        
+        
+        # deq = deque()
+        # if root: deq.append(root)
+        # prev_node = None
+        # while deq:
+        #     for ii in range(len(deq)):
+        #         curr_node = deq.popleft()
+        #         if curr_node.left: deq.append(curr_node.left)
+        #         if curr_node.right: deq.append(curr_node.right)
+        #         if prev_node: prev_node.next = curr_node
+        #         prev_node = curr_node
+        #     curr_node.next = None
+        #     prev_node = None
+        # return root
+        
+        
+        
+        if not root:
+            return root
+        
+        # Start with the root node. There are no next pointers
+        # that need to be set up on the first level
+        leftmost = root
+        
+        # Once we reach the final level, we are done
+        while leftmost.left:
+            
+            # Iterate the "linked list" starting from the head
+            # node and using the next pointers, establish the 
+            # corresponding links for the next level
+            head = leftmost
+            while head:
+                
+                # CONNECTION 1
+                head.left.next = head.right
+                
+                # CONNECTION 2
+                if head.next:
+                    head.right.next = head.next.left
+                                                                                                                                             
+                # Progress along the list (nodes on the current level)
+                head = head.next
+            
+            # Move onto the next level
+            leftmost = leftmost.left
+        
+        return root 
