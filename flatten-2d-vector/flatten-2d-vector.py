ead482c66dbf2e706618182b45a8eac55036557c
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-04-14 23:36:27 -0500

    Time: 76 ms (86.66%), Space: 19.2 MB (91.43%) - LeetHub

--- flatten-2d-vector/flatten-2d-vector.py
+++ flatten-2d-vector/flatten-2d-vector.py
@@ -1,50 +1,56 @@
+# #Brute Force - O(N + V) initialization time and O(N) space
+# #where N == number of ints, V == # of total lists in vec (including empty ones)
+# class Vector2D:
+
+#     def __init__(self, vec: List[List[int]]):#[[[],[3]]]
+#         print(vec)
+#         self.arr = []
+#         self.curr_pos = 0
+#         for ls in vec:
+#             for num in ls:
+#                 self.arr.append(num)
+#         print(self.arr)
+#     def next(self) -> int:
+#         self.curr_pos += 1
+#         return self.arr[self.curr_pos - 1]
+        
+        
+#     def hasNext(self) -> bool:
+#         return self.curr_pos < len(self.arr)
+# ["Vector2D","hasNext","next","hasNext"]
+# [[[[],[3]]],[],[],[]]
+
 class Vector2D:
 
     def __init__(self, vec: List[List[int]]):
-        self.arr = []
-        self.curr_pos = 0
-        for ls in vec:
-            for num in ls:
-                self.arr.append(num)
+        self.vec = vec
+        self.curr_x = 0
+        self.curr_y = -1
+        self.curr_ls = vec[0] if vec else None
+        self.find_next_num()
+        
+    def find_next_num(self):
+        if self.curr_ls is None: return
+        self.curr_ls = self.vec[self.curr_x]
+        
+        if self.curr_y + 1 >= len(self.curr_ls):
+            self.curr_y = 0
+            self.curr_x += 1
+            while self.curr_x < len(self.vec) and not self.vec[self.curr_x]:
+                self.curr_x += 1
+        else:
+            self.curr_y += 1
         
     def next(self) -> int:
-        self.curr_pos += 1
-        return self.arr[self.curr_pos - 1]
         
+
+        self.curr_ls = self.vec[self.curr_x]
+        to_ret = self.curr_ls[self.curr_y]
+        self.find_next_num()
+        return to_ret
         
     def hasNext(self) -> bool:
-        return self.curr_pos < len(self.arr)
-
-
-# class Vector2D:
-
-#     def __init__(self, vec: List[List[int]]):
-#         self.vec = vec
-#         self.curr_x = 0
-#         self.curr_y = 0
-#         self.curr_ls = vec[0] if vec else []
-#         print(len(self.vec))
-#         # [[[]]]
-#         # [[]]
-#         # [[[1,2],[3],[4]]] - [[[[1,2],[3],[4]]],[],[],[],[],[],[],[]]
-#         # [[[],[3]]] - [[[[],[3]]],[],[],[]]
-#     def next(self) -> int:
-#         self.curr_ls = self.vec[self.curr_x]
-#         to_ret = self.curr_ls[self.curr_y]
-        
-#         if self.curr_y + 1 >= len(self.curr_ls):
-#             self.curr_y = 0
-#             self.curr_x += 1
-#         else:
-#             self.curr_y += 1
-#         return to_ret
-        
-#     def hasNext(self) -> bool:
-#         if not self.vec or not self.vec[0]: return False
-#         if self.curr_x == len(self.vec):
-#             return False
-#         return True
-
+        return self.curr_x < len(self.vec)
 
 # Your Vector2D object will be instantiated and called as such:
 # obj = Vector2D(vec)
