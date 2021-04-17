7d5306a1cb5b85035dfc94eca5b89057d973dbec
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-04-17 09:01:51 -0500

    Time: 160 ms (83.09%), Space: 15.6 MB (8.42%) - LeetHub

--- majority-element/majority-element.py
+++ majority-element/majority-element.py
@@ -1,5 +1,19 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counts = collections.Counter(nums)
-        return max(counts,key = lambda xx: counts[xx])
+        #Boyer-Moore voting algorithm
+        #O(N) time and O(1) space
+        count = 0
+        candidate = None
         
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if candidate == num else -1)
+        return candidate
+    
+        #Hashmap - O(N) time and space
+        # counts = collections.Counter(nums)
+        # return max(counts,key=lambda xx:counts[xx])
+        
+
+            
