faa30a2becb2c45b5d1956bdf314bff1015d20eb
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-05-01 15:22:34 -0500

    Time: 100 ms (66.93%), Space: 17.4 MB (5.36%) - LeetHub

---
+++ all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -0,0 +1,16 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+
+        @lru_cache(maxsize=None)
+        def track(node):
+            if node == len(graph) - 1:
+                return [[node]]
+            ret = []
+            for child_node in graph[node]:
+                for paths in track(child_node):
+                    if paths:
+                        ret.append([node] + paths)
+            return ret
+                    
+        
+        return track(0)
