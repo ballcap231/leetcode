23ee55de9eb56920513ed96d348063d846356bdd
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-05-01 15:24:04 -0500

    Time: 104 ms (37.70%), Space: 17.6 MB (5.36%) - LeetHub

--- all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -1,6 +1,6 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-
+        #DP solution - O(N * 2^N) time and space
         @lru_cache(maxsize=None)
         def track(node):
             if node == len(graph) - 1:
@@ -11,6 +11,5 @@ class Solution:
                     if paths:
                         ret.append([node] + paths)
             return ret
-                    
-        
+
         return track(0)
