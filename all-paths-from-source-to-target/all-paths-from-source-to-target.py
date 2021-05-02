f664c6f744114a22a954c7c7dc889a9de9b0502c
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-05-01 20:50:13 -0500

    Time: 100 ms (66.93%), Space: 15.5 MB (74.48%) - LeetHub

--- all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -1,15 +1,36 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        #DP solution - O(N * 2^N) time and space
-        @lru_cache(maxsize=None)
-        def track(node):
-            if node == len(graph) - 1:
-                return [[node]]
-            ret = []
+        #Backtracking solution
+        #O(N * 2^N) - very loose time and space bound
+        #Same complexity analysis as that for a superset of nodes
+        #https://leetcode.com/problems/subsets/
+        ret = []
+        curr_arr = []
+        target = len(graph) - 1
+        def backtrack(node):
+            if node == target:
+                return True
+            curr_arr.append(node)
             for child_node in graph[node]:
-                for paths in track(child_node):
-                    if paths:
-                        ret.append([node] + paths)
-            return ret
+                if backtrack(child_node):
+                    ret.append(curr_arr + [target])
+            curr_arr.pop()
+            return False
+        backtrack(0)
+        return ret
+        
+        
+        
+#         #DP solution - O(N * 2^N) time and space
+#         @lru_cache(maxsize=None)
+#         def track(node):
+#             if node == len(graph) - 1:
+#                 return [[node]]
+#             ret = []
+#             for child_node in graph[node]:
+#                 for paths in track(child_node):
+#                     if paths:
+#                         ret.append([node] + paths)
+#             return ret
 
-        return track(0)
+#         return track(0)
