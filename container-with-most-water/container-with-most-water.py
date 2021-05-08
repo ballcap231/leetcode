137e060c01ecd1aed3ac86a3a95b1165674990ce
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-05-07 23:23:59 -0500

    Time: 732 ms (61.38%), Space: 26.3 MB (93.91%) - LeetHub

--- container-with-most-water/container-with-most-water.py
+++ container-with-most-water/container-with-most-water.py
@@ -1,5 +1,6 @@
 class Solution:
-    def maxArea(self, height: List[int]) -> int:
+    def maxArea(self, height: List[int]) -> int:       
+        # O(N) time and O(1) space - 2 pointer approach
         L,R = 0, len(height) - 1
         max_height = 0
         while L < R:
@@ -9,3 +10,10 @@ class Solution:
             else:
                 R -= 1
         return max_height
+        
+        # #O(N^2) time and O(1) space - Brute Force
+        # max_height = 0
+        # for L in range(len(height) - 1):
+        #     for R in range(L + 1, len(height)):
+        #         max_height = max(max_height, min(height[R], height[L]) * (R - L))
+        # return max_height 
