6a4521c26bd4bf51cd9de5326f75f42fe1a42ffb
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-05-16 10:43:57 -0500

    Time: 56 ms (82.67%), Space: 14.3 MB (51.08%) - LeetHub

--- combination-sum/combination-sum.py
+++ combination-sum/combination-sum.py
@@ -1,31 +1,24 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-#         sorted_candidates = sorted(candidates, reverse = True)
         
-#         for val in range(len(candidates)):
-            
-#         def n_choose_k(pos, k, res_arr):
-#             for pos, cand in enumerate(candidates):
-#                 cum_sum = cand
-#                 while cum_sum  < target:
-                    
-            
-        
-#         for k in range(n):
-#             n_choose_k(k,[])
-        
-        
-        # sorted_candidates = sorted(candidates, reverse = True)
+        #N-ary DFS using backtracking 
+        #O(N^(T/M)) time and O(T/M) space 
+        #where T is target, M is minimum value in candidates and
+        #N is the length of candidates
+        self.sorted_candidates = sorted(candidates, reverse = True)
         ans = []
-        def backtrack(candidates, cum_sum, stack):
-            for count, cand in enumerate(candidates):
+        def backtrack(start_pos, cum_sum, stack):
+            for count in range(start_pos, len(self.sorted_candidates)):
+                cand = self.sorted_candidates[count]
                 if cum_sum + cand > target:
                     continue
                 if cum_sum + cand == target:
                     ans.append(stack + [cand])
                     continue
-                backtrack(candidates[count:], cum_sum + cand, stack + [cand])
+                backtrack(count, cum_sum + cand, stack + [cand])
                 
                 
-        backtrack(candidates, 0, [])
+        backtrack(0, 0, [])
         return ans
+    
+    
