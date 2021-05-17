6f0f76d872dea18cd24b6dc862092ced1e4c828d
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-05-17 00:34:48 -0500

    Time: 36 ms (86.09%), Space: 14.5 MB (42.12%) - LeetHub

--- permutations/permutations.py
+++ permutations/permutations.py
@@ -1,5 +1,7 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        #O(N! / (N - N)! * N) or O(N! * N) time
+        #O(N! * N) space
         res = []
         
         def backtrack(first_pos):
@@ -10,9 +12,7 @@ class Solution:
                     nums[pos],nums[first_pos] = nums[first_pos], nums[pos]
 
                     backtrack(first_pos + 1)
-
                     nums[pos],nums[first_pos] = nums[first_pos], nums[pos]
-        
         backtrack(0)
         return res
             
@@ -29,20 +29,5 @@ class Solution:
         
         
         
-#         # my solution (not LC soln).... what time complexity is this??? - may not be best.
-#         if not nums:
-#             return [[]]
-#         ls = []
-        
-#         for count, val in enumerate(nums):
-#             if count != 0:
-#                 for sub_count, sub_ls in enumerate(ls):
-#                     if sub_count == 0:
-#                         new_ls = [sub_ls[:x] + [val] + sub_ls[x:] for x in range(len(sub_ls) + 1)]
-#                     else:
-#                         new_ls += [sub_ls[:x] + [val] + sub_ls[x:] for x in range(len(sub_ls) + 1)]
-#                 ls = new_ls
-#             else:
-#                 ls += [[val]]
-#         return ls
+
         
