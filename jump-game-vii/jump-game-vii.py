84247b555fe4c8f72dbf1e4dec7603f481088185
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-05-23 15:14:09 -0500

    Time: 364 ms (22.22%), Space: 18.1 MB (55.56%) - LeetHub

--- jump-game-vii/jump-game-vii.py
+++ jump-game-vii/jump-game-vii.py
@@ -1,12 +1,13 @@
 class Solution:
     def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+#         #O(N * (maxJump - minJump)) time and O(N) space
 #         if s[-1] != '0':
 #             return False
         
 #         @lru_cache(maxsize = None)
 #         def DFS(pos):
 #             if pos == len(s) - 1:
-#                 return s[pos] == '0'
+#                 return True
 #             if s[pos] == '1':
 #                 return False
 #             for new_pos in reversed(range(pos + minJump, min(pos + maxJump + 1, len(s)))):
@@ -14,16 +15,20 @@ class Solution:
 #             return False
         
 #         return DFS(0)
-    
-        q, farthest = deque([0]), 0
+
+        #O(N) time and O(N) space
+        deq, farthest = deque([0]), 0
         
-        while q:
-            i = q.popleft()
-            start = max(i + minJump, farthest + 1)
-            for j in range(start, min(len(s), i + maxJump + 1)):
-                if s[j] == "0":
-                    q.append(j)
-                    if j == len(s) - 1:
+        while deq:
+            pos = deq.popleft()
+            start =  max(farthest + 1, pos + minJump)
+            for new_pos in range(start, min(len(s), pos + maxJump + 1)):
+                if s[new_pos] == '0':
+                    deq.append(new_pos)
+                    if new_pos == len(s) - 1:
                         return True
-            farthest = max(farthest, i + maxJump)
+            farthest = max(farthest, pos + maxJump)
         return False
+                    
+            
+            
