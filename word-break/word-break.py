38aae1c290acc59edd9d33c5363182787ae7443b
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-05-26 23:12:48 -0500

    Time: 36 ms (79.04%), Space: 14.4 MB (68.86%) - LeetHub

--- word-break/word-break.py
+++ word-break/word-break.py
@@ -1,22 +1,62 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        len_word = len(s)
-        dictionary = set(wordDict)
-
-        @lru_cache(maxsize=None)
-        def check_substring(start):
-            if start == len_word:
-                return True
-            for curr_pos in range(start + 1, len_word + 1):
-                if s[start:curr_pos] in dictionary and check_substring(curr_pos):
-                    return True
-            return False
+        words = set(wordDict)
+        deq = deque()
+        is_word = [0 for _ in range(len(s) + 1)]
+        is_word[0] = 1
         
-        return check_substring(0)
+        for right in range(1, len(s) + 1):
+            for left in range(right):
+                if is_word[left] and s[left:right] in words:
+                    is_word[right] = 1
+                    break
+        return is_word[-1]
+        
+        
+        
+        
+#         #Brute force using Backtracking
+#         #Worst case example: s = 'aaaaab' and wordDict = {'a','aa','aaa','aaaa'}
+#         #O(2^N) time and O(N + |wordDict|) space complexity
+#         len_word = len(s)
+#         dictionary = set(wordDict)
+
+#         def check_substring(start):
+#             if start == len_word:
+#                 return True
+#             for curr_pos in range(start + 1, len_word + 1):
+#                 #O(N) operation to slice string
+#                 if s[start:curr_pos] in dictionary and check_substring(curr_pos):
+#                     return True
+#             return False
+        
+#         return check_substring(0)
+        
+        
+        
+        
+#         #Worst case example: s = 'aaaaab' and wordDict = {'a','aa','aaa','aaaa'}
+#         #O(N^3) time and O(N + |wordDict|) space complexity
+#         len_word = len(s)
+#         dictionary = set(wordDict)
+
+#         @lru_cache(maxsize=None)#O(N^2) operation to check all combinations of substrings
+#         def check_substring(start):
+#             if start == len_word:
+#                 return True
+#             for curr_pos in range(start + 1, len_word + 1):
+#                 #O(N) operation to slice string
+#                 if s[start:curr_pos] in dictionary and check_substring(curr_pos):
+#                     return True
+#             return False
+        
+#         return check_substring(0)
 
 
 
-
+#O(N^2 + MK) time and O(N + MK) space
+# "Assuming the dictionary of words to be K with average length of M the time and space complexity would be O(MK)"
+#https://leetcode.com/problems/word-break/discuss/729064/Python-or-DP-%2B-Trie-or-O(N2)-O(N)
 # class Node:
 #     def __init__(self):
 #         self.children = dict()
