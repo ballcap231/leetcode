e0ebcdc596e673da8fccf11426bc234001f64938
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-06-06 13:20:54 -0500

    Time: 956 ms (100.00%), Space: 21.3 MB (100.00%) - LeetHub

--- reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.py
+++ reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.py
@@ -1,22 +1,31 @@
 class Solution:
     def reductionOperations(self, nums: List[int]) -> int:
-        counts = Counter(nums)
-        if len(counts) == 1:
-            return 0
-        min_val = min(nums)
+        res = 0
+        nums.sort(reverse = True)
+        for ii in range(len(nums) - 1):
+            if nums[ii] != nums[ii + 1]:
+                res += ii + 1
+        return res
         
-        hq = []
-        moves = 0
-        #Sorting values in buckets
-        for val, counts in counts.items():
-            heapq.heappush(hq, (-val, counts))
         
-        #Loop until the only value in heap is the minimum value - only minimum val left
-        while hq[0][0] != -min_val:
-            curr_val, curr_count = heapq.heappop(hq)
-            moves += curr_count
-            next_val, next_count = heapq.heappop(hq)
-            heapq.heappush(hq, (next_val, next_count + curr_count))
         
-        return moves
+#         counts = Counter(nums)
+#         if len(counts) == 1:
+#             return 0
+#         min_val = min(nums)
+        
+#         hq = []
+#         moves = 0
+#         #Sorting values in buckets
+#         for val, counts in counts.items():
+#             heapq.heappush(hq, (-val, counts))
+        
+#         #Loop until the only value in heap is the minimum value - only minimum val left
+#         while hq[0][0] != -min_val:
+#             curr_val, curr_count = heapq.heappop(hq)
+#             moves += curr_count
+#             next_val, next_count = heapq.heappop(hq)
+#             heapq.heappush(hq, (next_val, next_count + curr_count))
+        
+#         return moves
             
