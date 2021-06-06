139caba8ff8fcdef5233006f59136d4bbb58e08c
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-06-06 11:48:37 -0500

    Time: 1792 ms (38.14%), Space: 14.4 MB (37.05%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.py
+++ longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,32 +1,20 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        table = [[False for _ in range(len(s))] for _ in range(len(s))]
-        max_length = 1
-        start = len(s) - 1
-        
-        #Making all chars on diagonal to be True - 1 character alone is a palindrome
-        for ii in range(len(s)):
-            table[ii][ii] = True
-        
-        #Checking if all chars of len 2 are palindromes
-        for ii in range(len(s) - 1):
-            if s[ii] == s[ii + 1]:
-                table[ii][ii + 1] = True
-                if max_length == 1:
-                    max_length = 2
-                    start = ii
-        
-        
-        for width in range(3, len(s) + 1):
-            for i in range(len(s) - width + 1):
-                j = i + width - 1
-                if table[i + 1][j - 1] and s[i] == s[j]:
-                    table[i][j] = True
-                    if width > max_length:
-                        max_length = width
-                        start = i
-        
-        return s[start:start + max_length]
+        # if s is "":
+        #     return s
+        # res = ""
+        # dp = [[None for i in range(len(s))] for j in range(len(s))]
+        # for j in range(len(s)):
+        #     for i in range(j + 1):
+        #         if i == j:
+        #             dp[j][i] = True
+        #         elif j == i+1:
+        #             dp[j][i] = (s[i] == s[j])
+        #         else:
+        #             dp[j][i] = (dp[j-1][i+1] and s[i] == s[j])
+        #         if dp[j][i] and j - i + 1 > len(res):
+        #             res = s[i:j+1]
+        # return res
         
         
         
@@ -34,39 +22,83 @@ class Solution:
         
         
         
-#         if len(s) < 2:
-#             return s
-#         max_len = 1
-#         start_p = 0
-#         end_p = 0
-#         def palindrome_calc(left_index, right_index):
-#             while left_index >= 0 and right_index < len(s) and s[left_index] == s[right_index]:
-#                 left_index -= 1
-#                 right_index += 1
-#             return left_index + 1, right_index - 1
         
-#         def check(index):
-#             #return longest indices that result in a palindrome
-#             max_l, max_r = index, index
-#             #checking index 
-#             L1_pc, R1_pc = palindrome_calc(index, index)
-#             max_l,max_r = L1_pc, R1_pc
-#             #checking index + 1
-#             if s[index] == s[index + 1]:
-#                 L2_pc, R2_pc = palindrome_calc(index, index+1)
-#                 if R2_pc - L2_pc > R1_pc - L1_pc:
-#                     max_l,max_r = L2_pc,R2_pc
+        
+        
+#         #O(N^2) time and space - Bottom Up DP
+#         table = [[False for _ in range(len(s))] for _ in range(len(s))]
+#         max_length = 1
+#         start = len(s) - 1
+        
+#         #Making all chars on diagonal to be True - 1 character alone is a palindrome
+#         for ii in range(len(s)):
+#             #Making all chars on diagonal to be True - 1 character alone is a palindrome
+#             table[ii][ii] = True
+#             #Checking if all chars of len 2 are palindromes
+#             if ii < len(s) - 1:
+#                 if s[ii] == s[ii + 1]:
+#                     table[ii][ii + 1] = True
+#                     if max_length == 1:
+#                         max_length = 2
+#                         start = ii
+#         # #Checking if all chars of len 2 are palindromes
+#         # for ii in range(len(s) - 1):
+#         #     if s[ii] == s[ii + 1]:
+#         #         table[ii][ii + 1] = True
+#         #         if max_length == 1:
+#         #             max_length = 2
+#         #             start = ii
+        
+#         for width in range(3, len(s) + 1):
+#             for i in range(len(s) - width + 1):
+#                 # print(width, i)
+#                 j = i + width - 1
+#                 if table[i + 1][j - 1] and s[i] == s[j]:
+#                     table[i][j] = True
+#                     if width > max_length:
+#                         max_length = width
+#                         start = i
+#         return s[start:start + max_length]
+        
+        
+        
+        
+        
+        
+        
+        if len(s) < 2:
+            return s
+        max_len = 1
+        start_p = 0
+        end_p = 0
+        def palindrome_calc(left_index, right_index):
+            while left_index >= 0 and right_index < len(s) and s[left_index] == s[right_index]:
+                left_index -= 1
+                right_index += 1
+            return left_index + 1, right_index - 1
+        
+        def check(index):
+            #return longest indices that result in a palindrome
+            max_l, max_r = index, index
+            #checking index 
+            L1_pc, R1_pc = palindrome_calc(index, index)
+            max_l,max_r = L1_pc, R1_pc
+            #checking index + 1
+            if s[index] == s[index + 1]:
+                L2_pc, R2_pc = palindrome_calc(index, index+1)
+                if R2_pc - L2_pc > R1_pc - L1_pc:
+                    max_l,max_r = L2_pc,R2_pc
 
-#             return max_l,max_r
+            return max_l,max_r
             
 
-#         for index in range(len(s) - 1):
-#             L, R = check(index)
+        for index in range(len(s) - 1):
+            L, R = check(index)
             
-#             if R - L + 1 > max_len:
-#                 max_len = R - L + 1
-#                 start_p = L
-#                 end_p = R
+            if R - L + 1 > max_len:
+                max_len = R - L + 1
+                start_p = L
+                end_p = R
                 
             
-#         return s[start_p:end_p + 1]
+        return s[start_p:end_p + 1]
