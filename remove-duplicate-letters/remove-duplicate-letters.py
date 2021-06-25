a224b5c8f8b4f3aa3bbfc8e8514d9e545fa84028
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-06-25 17:36:47 -0500

    Time: 32 ms (90.83%), Space: 14.5 MB (19.82%) - LeetHub

---
+++ remove-duplicate-letters/remove-duplicate-letters.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        # Stack
+        #O(N) time and O(N) space
+        seen_chars = set()
+        furthest_pos = {char:pos for pos, char in enumerate(s)}
+        stack = []
+        for pos in range(len(s)):
+            if s[pos] not in seen_chars:
+                while stack and stack[-1] > s[pos] and furthest_pos[stack[-1]] > pos:
+                    seen_chars.discard(stack.pop())
+                stack.append(s[pos])
+                seen_chars.add(s[pos])
+        return ''.join(stack)
+        
