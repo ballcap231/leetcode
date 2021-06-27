90381fa42b5e6bf66fbb24060caa9a3dbe798f92
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-06-27 11:41:08 -0500

    Time: 48 ms (78.22%), Space: 15.4 MB (60.15%) - LeetHub

--- move-zeroes/move-zeroes.py
+++ move-zeroes/move-zeroes.py
@@ -3,12 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        #O(N) time and O(1) space
+        #O(N) time and O(1) space - 1 loop
         l = 0
         for r in range(len(nums)):
             if nums[r] != 0:
-                nums[l] = nums[r]
+                nums[l],nums[r] = nums[r], nums[l]
                 l += 1
-        for l in range(l, len(nums)):
-            nums[l] = 0
+        
+        # 2 Loops
+#         #O(N) time and O(1) space
+#         l = 0
+#         for r in range(len(nums)):
+#             if nums[r] != 0:
+#                 nums[l] = nums[r]
+#                 l += 1
+#         for l in range(l, len(nums)):
+#             nums[l] = 0
         
