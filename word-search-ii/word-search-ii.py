27956df0e067ba0465b827c2ffd646ec84454dee
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-07-01 18:09:08 -0500

    Time: 440 ms (77.62%), Space: 14.8 MB (18.69%) - LeetHub

--- word-search-ii/word-search-ii.py
+++ word-search-ii/word-search-ii.py
@@ -19,31 +19,39 @@ class Trie:
 
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        #O(N * M  * 3^K + P) time
+        #O(P) space
+        # N and M are length of width of board, K is max length of all strings in words, P is number of chars in words list
         trie = Trie()
         for word in words:
             trie.add_word(word)
-        self.found_words = set()
+        self.found_words = []
         x_len, y_len = len(board), len(board[0])
         moves = [(1,0),(-1,0),(0,1),(0,-1)]
-        def backtrack(xx, yy, node):
+        def backtrack(xx, yy, parent_node):
             if 0 > xx or xx >= x_len or 0 > yy or yy >= y_len or \
-                board[xx][yy] not in node.children:
+                board[xx][yy] not in parent_node.children:
                 return
             curr_char = board[xx][yy]
-            new_node = node.children[curr_char]
-            if new_node.is_word:
-                self.found_words.add(new_node.final_word)
-            
+            child_node = parent_node.children[curr_char]
+            if child_node.is_word:
+                self.found_words.append(child_node.final_word)
+                child_node.is_word = False
+                child_node.final_word = None
+
             board[xx][yy] = '#'
             for move in moves:
                 new_x, new_y = xx + move[0], yy + move[1]
-                backtrack(new_x, new_y, new_node)
+                backtrack(new_x, new_y, child_node)
             board[xx][yy] = curr_char
-            return
+            
+            if not child_node.is_word and not child_node.children:
+                parent_node.children.pop(curr_char)
+            
         for x_pos in range(x_len):
             for y_pos in range(y_len):
                 backtrack(x_pos, y_pos, trie.root)
-        return list(self.found_words)
+        return self.found_words
                 
             
             
