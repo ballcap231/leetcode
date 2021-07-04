5548e81f477db82729caf00671572ea997ca81bd
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-07-03 19:15:12 -0500

    Time: 28 ms (93.99%), Space: 14.5 MB (23.78%) - LeetHub

---
+++ longest-common-prefix/longest-common-prefix.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        min_str = min(len(xx) for xx in strs)
+        char_pos = 0
+        if len(strs) == 1: return strs[0]
+        
+        while char_pos < min_str:
+            for word_pos in range(1, len(strs)):
+                first_word, second_word = strs[word_pos - 1], strs[word_pos]
+                if first_word[char_pos] != second_word[char_pos]:
+                    return strs[0][:char_pos]
+            else:
+                char_pos += 1
+        
+        return strs[0][:char_pos]
