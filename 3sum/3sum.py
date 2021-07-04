f54a7a9c288a544883ea41d513974ee779c018da
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-07-04 00:52:10 -0500

    Time: 912 ms (63.41%), Space: 17.5 MB (72.85%) - LeetHub

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -1,9 +1,6 @@
 class Solution(object):
-    
     #At this point, list is sorted
     def twoSum(self,start,end, k):
-        if start == end:
-            return []
         res = []
         while start<end:
             s = self.nums[start]+self.nums[end]
@@ -30,7 +27,7 @@ class Solution(object):
         self.nums = sorted(nums)
         res = []
         
-        for i in range(len(self.nums)):
+        for i in range(len(self.nums) - 2):
             if i > 0  and self.nums[i] == self.nums[i - 1]:
                 continue
             k = -self.nums[i]
@@ -39,3 +36,21 @@ class Solution(object):
                 res.append([self.nums[i],t[0],t[1]])        
         
         return res
+    # def threeSum(self, nums):
+        # #Brute Force
+        # #O(N^3) time and O(1) auxillary space
+        # output = []
+        # nums.sort()
+        # if len(nums) < 3 or nums[0] >= 1 or nums[-1] < 0: return []
+        # soln = set()
+        # for xx in range(len(nums) - 2):
+        #     for yy in range(xx + 1, len(nums) - 1):
+        #         for zz in range(yy + 1, len(nums)):
+        #             if nums[xx] + nums[yy] + nums[zz] == 0:
+        #                 soln.add(tuple(sorted([nums[xx],nums[yy],nums[zz]])))
+        # return list(soln)
+    
+    
+    
+    
+    
