ff1dabe982c5a6097d5efa233c91c71ddf2cf2b7
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-07-08 10:32:39 -0500

    Time: 160 ms (12.91%), Space: 34.2 MB (8.18%) - LeetHub

--- group-anagrams/group-anagrams.py
+++ group-anagrams/group-anagrams.py
@@ -1,35 +1,41 @@
 from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dd = defaultdict(list)
-        output = []
-        for pos, word in enumerate(strs):
-            counter = Counter(word)
-            # key = tuple(sorted(counter.items()))
-            key = frozenset(counter.items())
-            dd[key].append(pos)
-        for key,val in dd.items():
-            new_ls = []
-            for ii in val:
-                new_ls.append(strs[ii])
-            output.append(new_ls)
-        return output
-        
-        
-        # ans = collections.defaultdict(list)
+        # #Bucket Sort on ASCII values
+        # #O(N * K) time where N == |strs|, K == length of words ins strs
+        # #O(N * K) space
+        # dd = defaultdict(list)
         # for s in strs:
         #     count = [0] * 26
         #     for c in s:
         #         count[ord(c) - ord('a')] += 1
-        #     ans[tuple(count)].append(s)
-        # return ans.values()        
-        
+        #     dd[tuple(count)].append(s)
+        # return dd.values()
 
         
-        # dd = defaultdict(list)
-        # for s in strs:
-        #     dd2 = defaultdict(int)
-        #     for c in s:
-        #         dd2[c] += 1
-        #     dd[frozenset(dd2.items())].append(s)
-        # return dd.values()
+        dd = defaultdict(list)
+        for s in strs:
+            #Can make array of 200 if you can't remember relative ordering of ascii values
+            count = [0] * 200
+            for c in s:
+                count[ord(c)] += 1
+            dd[tuple(count)].append(s)
+        return dd.values()
+    
+        
+        
+#         #frozenset on strings
+#         #O(N * K) time where N == |strs|, K == length of words ins strs
+#         #O(N * K) space
+#         dd = defaultdict(list)
+#         for s in strs:
+#             dd[frozenset(Counter(s).items())].append(s)
+#         return dd.values()
+    
+#         #Sorting on strings
+#         #O(N * K * log(K)) time where N == |strs|, K == length of words ins strs
+#         #O(N * K) space
+#         dd = defaultdict(list)
+#         for s in strs:
+#             dd[tuple(sorted(Counter(s).items()))].append(s)
+#         return dd.values()
