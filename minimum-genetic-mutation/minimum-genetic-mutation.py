4f3a6fbe782b4ee1dc200cf7e1d5610e2dd4cff1
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-07-08 20:00:52 -0500

    Time: 36 ms (15.71%), Space: 14.5 MB (28.76%) - LeetHub

--- minimum-genetic-mutation/minimum-genetic-mutation.py
+++ minimum-genetic-mutation/minimum-genetic-mutation.py
@@ -1,45 +1,67 @@
-from itertools import combinations
-from collections import defaultdict, deque
-class Solution:
-    def is_mutation(self, gene1, gene2):
-        #Returns if Hamming Distance is less than 2
-        diff = 0
-        pos = 0
-        while diff < 2 and pos < len(gene1):
-            if gene1[pos] != gene2[pos]:
-                diff += 1
-            pos += 1
-        return diff < 2
+# from itertools import combinations
+# from collections import defaultdict, deque
+# class Solution:
+#     def is_mutation(self, gene1, gene2):
+#         #Returns if Hamming Distance is less than 2
+#         diff = 0
+#         pos = 0
+#         while diff < 2 and pos < len(gene1):
+#             if gene1[pos] != gene2[pos]:
+#                 diff += 1
+#             pos += 1
+#         return diff < 2
         
-    def minMutation(self, start: str, end: str, bank: List[str]) -> int:
-        #O(V^2 + V + E) == O(V^2) time and O(V^2) space, 
-        # where V = |bank| and E = number of genes pairs that differs by 1 char in bank
-        # but V^2 is always larger than E since V^2 is all gene pairs possible in bank
-        out_nodes = defaultdict(list)
-        graph = bank + [start]
-        #Takes O(V^2) time and space to build all possible gene pair combinations
-        combos = combinations(graph, r = 2)
+#     def minMutation(self, start: str, end: str, bank: List[str]) -> int:
+#         # BFS on combinations of input strings
+#         # O(V^2 + V + E) == O(V^2) time and O(V^2) space, 
+#         # where V = |bank| and E = number of genes pairs that differs by 1 char in bank
+#         # but V^2 is always larger than E since V^2 is all gene pairs possible in bank
+#         out_nodes = defaultdict(list)
+#         graph = bank + [start]
+#         #Takes O(V^2) time and space to build all possible gene pair combinations
+#         combos = combinations(graph, r = 2)
         
-        for combo in combos:
-            if self.is_mutation(combo[0], combo[1]):
-                out_nodes[combo[0]].append(combo[1])
-                out_nodes[combo[1]].append(combo[0])
+#         for combo in combos:
+#             if self.is_mutation(combo[0], combo[1]):
+#                 out_nodes[combo[0]].append(combo[1])
+#                 out_nodes[combo[1]].append(combo[0])
 
+#         dq = deque([(start, 0)])
+#         visited = set([start])
+#         # BFS - takes O(V + E) traversal and O(V) space
+#         while dq:
+#             curr_node, depth = dq.popleft()
+#             for node in out_nodes[curr_node]:
+#                 if node == end:
+#                     return depth + 1
+#                 if node not in visited:
+#                     visited.add(node)
+#                     dq.append((node, depth + 1))
+#         return -1
+
+from collections import deque
+class Solution:
+    def minMutation(self, start: str, end: str, bank: List[str]) -> int:
+        graph = set(bank)
         dq = deque([(start, 0)])
-        visited = set([start])
-        # BFS - takes O(V + E) traversal and O(V) space
+        visited = set()
+        
         while dq:
             curr_node, depth = dq.popleft()
-            for node in out_nodes[curr_node]:
-                if node == end:
-                    return depth + 1
-                if node not in visited:
-                    visited.add(node)
-                    dq.append((node, depth + 1))
-                    
+            for pos in range(len(curr_node)):
+                for char in 'ACGT':
+                    new_gene = curr_node[:pos] + char + curr_node[pos + 1:]
+                    if new_gene not in visited:
+                        if new_gene in graph:
+                            if new_gene == end:
+                                return depth + 1
+                            visited.add(new_gene)
+                            dq.append((new_gene, depth + 1))
         return -1
             
             
+        
+        
         
         
         
