bcc71857116f5fec021cb051a8794eaca23fafb9
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-07-10 16:02:20 -0500

    Time: 72 ms (84.94%), Space: 15.5 MB (89.86%) - LeetHub

--- k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.py
+++ k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.py
@@ -1,28 +1,33 @@
 class Solution:
     def findPairs(self, nums: List[int], k: int) -> int:
-        nums_sorted = sorted(nums)
-        set_nums = set()
-        count = 0
-        r = 0
-        while r < len(nums):
-            curr_num = nums_sorted[r]
-            if curr_num - k in set_nums or curr_num + k in set_nums:
-                count += 1
-                set_nums.add(curr_num)
-                while r < len(nums) and nums_sorted[r] == curr_num:
-                    r += 1
-                continue
-            set_nums.add(curr_num)
-            r += 1
-        return count
-        
-        
-        
+        #O(N) time and space
+        result = 0
+        counter = Counter(nums)
 
+        for x in counter:
+            if k > 0 and x + k in counter:
+                result += 1
+            elif k == 0 and counter[x] > 1:
+                result += 1
+            elif k < 0 and x - k in counter:
+                result += 1
+        return result
         
-#         set_nums = set()
-#         count = 0
         
-#         for num in set_nums:
-#             if num - k in set_nums or num + k in set_nums:
-                
+        
+        # #O(NlogN) time and O(N) space
+        # nums_sorted = sorted(nums)
+        # set_nums = set()
+        # count = 0
+        # r = 0
+        # while r < len(nums):
+        #     curr_num = nums_sorted[r]
+        #     if curr_num - k in set_nums or curr_num + k in set_nums:
+        #         count += 1
+        #         set_nums.add(curr_num)
+        #         while r < len(nums) and nums_sorted[r] == curr_num:
+        #             r += 1
+        #         continue
+        #     set_nums.add(curr_num)
+        #     r += 1
+        # return count
