477b4f497294d7248354509143f7f8f7a7992fc9
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-07-11 20:21:53 -0500

    Time: 104 ms (53.41%), Space: 19.8 MB (7.82%) - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.py
+++ top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,26 +1,21 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-#         # Bucket Sort-Like, O(N) time and space
-#         # No sorting necessary inside buckets
-#         # 'Sorting' happens by relative bucketing
-#         buckets = [[] for xx in range(len(nums))]
-#         counts = Counter(nums)
-#         for num, count in counts.items():
-#             buckets[count - 1].append(num)
-        
-#         ret_ls = []
-#         for bucket in buckets:
-#             for val in bucket:
-#                 ret_ls.append(val)
-#         return ret_ls[-k:]
-    
+        # Bucket Sort-Like, O(N) time and space
+        # No sorting necessary inside buckets
+        # 'Sorting' happens by relative bucketing - no insertion sort needed
+        buckets = [[] for xx in range(len(nums))]
         counts = Counter(nums)
-        most_common = [key_val_tuple[0] for key_val_tuple in counts.most_common(k)]
-        return most_common
+        for num, count in counts.items():
+            buckets[count - 1].append(num)
+        
+        ret_ls = []
+        for bucket in buckets:
+            for val in bucket:
+                ret_ls.append(val)
+        return ret_ls[-k:]
     
-    
-            
-#         #Heap
+
+#         #Smart Heap
 #         #O(N + KlogN) time and O(N) space, where K == k, N == |nums|
 #         counts = Counter(nums)
 #         count_ls = [(-values, keys) for keys, values in counts.items()]
@@ -30,3 +25,9 @@ class Solution:
 #         for _ in range(k):
 #             ret.append(heapq.heappop(count_ls)[1])
 #         return ret
+
+        # Counter.most_common - uses heapq.nlargest which is O(NlogK)
+        # O(NlogK) time and O(N) space
+        counts = Counter(nums)
+        most_common = [key_val_tuple[0] for key_val_tuple in counts.most_common(k)]
+        return most_common
