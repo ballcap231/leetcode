1ed88516cfb06e79f1c43d5f64b67ab02723ef07
ballcap231 <40708323+ballcap231@users.noreply.github.com>
2021-07-11 19:59:32 -0500

    Time: 160 ms (11.85%), Space: 18.8 MB (56.41%) - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.py
+++ top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,19 +1,27 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # Bucket Sort-Like, O(N) time and space
-        buckets = [[] for xx in range(len(nums))]
-        counts = Counter(nums)
-        for num, count in counts.items():
-            buckets[count - 1].append(num)
+#         # Bucket Sort-Like, O(N) time and space
+#         # No sorting necessary inside buckets
+#         # 'Sorting' happens by relative bucketing
+#         buckets = [[] for xx in range(len(nums))]
+#         counts = Counter(nums)
+#         for num, count in counts.items():
+#             buckets[count - 1].append(num)
         
-        ret_ls = []
-        for bucket in buckets:
-            for val in bucket:
-                ret_ls.append(val)
-        return ret_ls[-k:]
+#         ret_ls = []
+#         for bucket in buckets:
+#             for val in bucket:
+#                 ret_ls.append(val)
+#         return ret_ls[-k:]
+    
+        counts = Counter(nums)
+        most_common = [key_val_tuple[0] for key_val_tuple in counts.most_common(k)]
+        return most_common
+    
+    
             
 #         #Heap
-#         #O(KlogN) time and O(N) space, where K == k, N == |nums|
+#         #O(N + KlogN) time and O(N) space, where K == k, N == |nums|
 #         counts = Counter(nums)
 #         count_ls = [(-values, keys) for keys, values in counts.items()]
 #         heapq.heapify(count_ls)
